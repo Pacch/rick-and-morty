42dc7f291c752f3713358329a533ab968cadaddf
Harol <darksxd@live.com>
2021-08-31 05:59:49 -0500

    feature(component): adding Pagination basic

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { tw } from "twind";
+import Icon from "components/Icon";
+
+interface IPropsNavigation {
+  disabled?: boolean;
+  children?: React.ReactNode;
+  onClick: () => void;
+}
+
+const NavigationButton: React.FC<IPropsNavigation> = ({
+  children,
+  disabled,
+  onClick,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={tw` focus:outline-none ${
+        disabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
+
+interface Props {
+  currentPage: number;
+  totalPages?: number;
+  onChangeCurrentPage: (value: number) => void;
+}
+
+interface IParams {
+  value?: string | null;
+  max: number;
+}
+
+const Pagination: React.FC<Props> = ({
+  currentPage = 1,
+  totalPages = 1,
+  onChangeCurrentPage,
+}) => {
+  const [currentPageText, setCurrentPageText] = useState<string>("");
+
+  const handleChangePage = (value: number) => {
+    onChangeCurrentPage(value);
+  };
+
+  const formatInput = ({ value, max }: IParams): string => {
+    if (!value || !parseInt(value)) return "";
+    if (parseInt(value) > max) return max.toString();
+    const formatToNumber = parseInt(value);
+    const formatToString = formatToNumber.toString();
+    return formatToString;
+  };
+
+  const handleChangeInputPage = (e: React.FormEvent) => {
+    const searchInputValue = (e.target as HTMLInputElement).value;
+    const value = formatInput({
+      value: searchInputValue,
+      max: totalPages,
+    });
+    setCurrentPageText(value);
+  };
+
+  const handleGoToPage = () => {
+    handleChangePage(parseInt(currentPageText));
+    setCurrentPageText("");
+  };
+
+  const handleNextPage = () => {
+    handleChangePage(++currentPage);
+  };
+
+  const handlePrevPage = () => {
+    handleChangePage(--currentPage);
+  };
+
+  const isDisabledGoToPage = !currentPageText.length;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    handleGoToPage();
+  };
+
+  return (
+    <div
+      className={tw`flex items-center justify-center p-6 space-x-6 flex-col sm:flex-row `}
+    >
+      <div className={tw`flex items-center space-x-4`}>
+        <NavigationButton onClick={handlePrevPage} disabled={currentPage === 1}>
+          <Icon name="chevron-left" size={25} />
+        </NavigationButton>
+
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+
+        <NavigationButton
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          <Icon name="chevron-right" size={25} />
+        </NavigationButton>
+      </div>
+      <form className={tw`flex items-center`} onSubmit={handleSubmit}>
+        <label htmlFor="inputPage">
+          <span className={tw`text-gray-400 mr-[1ch]`}>Go to page</span>
+          <input
+            type="text"
+            value={currentPageText}
+            onChange={(e) => handleChangeInputPage(e)}
+            id="inputPage"
+            name="inputPage"
+            className={tw`appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-2 w-[40px] text-center`}
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={isDisabledGoToPage}
+          className={tw`flex items-center`}
+        >
+          <span className={tw`mr-[1ch] ml-[1ch]`}>Go</span>
+          <Icon name="chevron-right" className={tw`text-black`} />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Pagination;
