edcdd9da6931fa1d77268329fe2fd94493bf8c53
Harol <darksxd@live.com>
2021-08-31 05:56:15 -0500

    feature(component): adding Icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,44 @@
+import { tw } from "twind";
+import iconPaths from "./selection.json";
+
+interface IProps {
+  name: string;
+  size?: number;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+const Icon: React.FC<IProps> = ({
+  name,
+  size = 15,
+  className = "",
+  width,
+  height,
+}) => {
+  const getPath = (iconName: string): string => {
+    const icon = (iconPaths as any).icons.find(
+      (icon: any) => icon.properties.name === iconName
+    );
+
+    if (icon) {
+      return icon.icon.paths.join(" ");
+    } else {
+      return "";
+    }
+  };
+
+  return (
+    <svg
+      data-testid="cc-icon__svg"
+      className={tw`fill-current ${className}`}
+      width={width ? width : size}
+      height={height ? height : size}
+      viewBox={"0 0 1024 1024"}
+    >
+      <path d={getPath(name)} />
+    </svg>
+  );
+};
+
+export default Icon;
